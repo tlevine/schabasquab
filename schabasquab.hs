39e0867bdf36e3b1eb02556af617ca6da5190c57
Thomas Levine <_@thomaslevine.com>
2014-07-21 05:09:25 -0400

    -

--- schabasquab.hs
+++ schabasquab.hs
@@ -5,18 +5,23 @@
 -- Lists are ranges of unspecified length, with all elements being similar.
 -- The cell is the atomic range.
 data Range = HTuple [Range] | VTuple [Range]
-           | HList  [Range] | VList  [Range]
-           | Cell
+           | HList  Range | VList Range
+           | Cell String
 
 -- | Grids are two-dimensional data representations.
 data Grid = 
 
 -- | Unpack one level of a range.
-unpack :: (Grid, Range) -> (Grid, [Range])
-unpack grid (HTuple range:ranges) = (left (length range) grid, ranges)
-unpack grid (VTuple range:ranges) = (down (length range) grid, ranges)
+unpack :: Grid -> Range -> (Maybe String, [Range])
+unpack grid (HTuple range:ranges) = unpack (left (length range) grid) (HTuple ranges)
+unpack grid (VTuple range:ranges) = unpack (down (length range) grid) (VTuple ranges)
 unpack grid (HList range) = (
 unpack grid (VList range) = 
-unpack grid (Cell) = (left 1 $ down 1 $ grid, [])
+unpack grid (Cell value) = (Just value, [])
+
+tidytable :: Range
+tidytable = 
+type TidyTable =
+HList (VTuple [Cell, VList])
 
 main = do putStr "3"
