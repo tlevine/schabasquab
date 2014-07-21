a33876f01006a246f531a9bcbd56717ce41775da
Thomas Levine <_@thomaslevine.com>
2014-07-21 05:56:11 -0400

    -

--- schabasquab.hs
+++ schabasquab.hs
@@ -4,20 +4,28 @@
 -- Tuples are ranges of a particular length, with any sort of contained elements.
 -- Lists are ranges of unspecified length, with all elements being similar.
 -- The cell is the atomic range.
+data Sort = Header String
+          | Data String
+
 data Range = HTuple [Range] | VTuple [Range]
            | HList  Range | VList Range
-           | Cell String
+           | Cell (String -> Sort)
 
 -- | Grids are two-dimensional data representations.
 data Grid = 
 
+type Box = (Grid, Range)
+
+-- | Boxes contain either more boxes or values
+data Unpack = Boxes [Box] | 
+
 -- | Unpack one level of a range.
-unpack :: Grid -> Range -> (Maybe String, [Range])
-unpack grid (HTuple range:ranges) = unpack (left (length range) grid) (HTuple ranges)
-unpack grid (VTuple range:ranges) = unpack (down (length range) grid) (VTuple ranges)
-unpack grid (HList range) = (
-unpack grid (VList range) = 
-unpack grid (Cell value) = (Just value, [])
+unpack :: (Grid, Range) -> [(Grid, Range)]
+unpack (grid, (HTuple range:ranges)) = (unpack (keepLeft (length range)) grid) ++ (unpack (dropLeft (length range) grid) (HTuple ranges))
+unpack (grid, (VTuple range:ranges)) = (unpack (keepDown (length range)) grid) ++ (unpack (dropDown (length range) grid) (VTuple ranges))
+unpack (grid, (HList range)) = (
+unpack (grid, (VList range)) = 
+unpack (grid, (Cell sorter)) = sorter $ value (keepLeft 1 grid $ keepDown 1 grid)
 
 tidytable :: Range
 tidytable = 
