dcb5fc729798e316bf057bb06f2ec512845c7ba5
Thomas Levine <_@thomaslevine.com>
2014-07-20 20:37:16 -0400

    -

--- schabasquab.hs
+++ schabasquab.hs
@@ -4,9 +4,16 @@
 -- Tuples are ranges of a particular length, with any sort of contained elements.
 -- Lists are ranges of unspecified length, with all elements being similar.
 -- The cell is the atomic range.
-
 data Range = HTuple [Range] | VTuple [Range]
            | HList  [Range] | VList  [Range]
-           | Cell
+           | Cell String
+
+-- | Unpack one level of a range.
+unpack :: Range -> Range -> [Range]
+unpack grid (HTuple range:ranges) = ((left (length range) grid) range)
+unpack grid (VTuple range:ranges) = ((down (length range) grid) range)
+unpack grid (HList range) = 
+unpack grid (VList range) = 
+unpack grid (Cell value) = (value, [])
 
 main = do putStr "3"
