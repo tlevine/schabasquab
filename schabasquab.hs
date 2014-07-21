2ac304f521bdeaa5f05d3b2c741d77f57ffbc3a3
Thomas Levine <_@thomaslevine.com>
2014-07-20 20:17:42 -0400

    -

--- schabasquab.hs
+++ schabasquab.hs
@@ -5,20 +5,8 @@
 -- Lists are ranges of unspecified length, with all elements being similar.
 -- The cell is the atomic range.
 
-type Cell = String
-type HLine = [Cell]
-type VLine = [Cell]
-
-data Range = 
-
-data Range = V
-
-
-{-
 data Range = HTuple [Range] | VTuple [Range]
-           | HList [Range] | VList [HRange]
-           | List Range
+           | HList [Range] | VList [Range]
            | Cell
--}
 
 main = do putStr "3"
