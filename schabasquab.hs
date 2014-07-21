05d299bef44ce317b9926cee44928cc6ab162ed6
Thomas Levine <_@thomaslevine.com>
2014-07-20 20:16:42 -0400

    -

--- schabasquab.hs
+++ schabasquab.hs
@@ -1,9 +1,24 @@
-Cell
+{-# LANGUAGE DataKinds #-}
 
--- Vertical
-VTuple
-VList
+-- | Ranges are one-dimensional.
+-- Tuples are ranges of a particular length, with any sort of contained elements.
+-- Lists are ranges of unspecified length, with all elements being similar.
+-- The cell is the atomic range.
 
--- Horizontal
-HTuple
-HList
+type Cell = String
+type HLine = [Cell]
+type VLine = [Cell]
+
+data Range = 
+
+data Range = V
+
+
+{-
+data Range = HTuple [Range] | VTuple [Range]
+           | HList [Range] | VList [HRange]
+           | List Range
+           | Cell
+-}
+
+main = do putStr "3"
