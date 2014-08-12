2651239a6709d4e48ba1848bd0d583fb06bcc572
Thomas Levine <_@thomaslevine.com>
2014-08-12 23:11:02 +0000

    -

--- schabasquab.hs
+++ schabasquab.hs
@@ -7,8 +7,10 @@ type CellSpec = Cell -> Bool
 data GridSpec = GridSpec (Box CellSpec)
 
 -- | Construct a box. Check that the component rows are all of the same length.
-assembleBox :: [[a]] -> Maybe Box a
-assembleBox listing = Box listing
+assembleBox :: a => [[a]] -> Maybe (Box a)
+assembleBox (firstRow:rows)
+  | all (\row -> length row == length firstRow) rows = Just (firstRow:rows)
+  | otherwise = Nothing
 
 -- | The width of a box
 width :: Box a -> Int
@@ -49,56 +51,12 @@ matches (HList grammar) grid = matches grammar (takeColumns w grid) && matches g
 matches (VList grammar) grid = matches grammar (takeRows h grid) && matches grammar (dropRows h grid)
   where
     h = height grammar
+{-
 matches (Atom (GridSpec gridspec)) grid
   | not (validDimensions = width gridspec == width grid && height gridspec == height grid) = False
   | height gridspec == 0 = True
   | height gridspec == 1 = (head (head gridspec)) (head (head grid)) && validate (tail (head gridspec)) (tail (head grid))
   | otherwise = validate (tail gridspec) (tail grid)
-
-
-
-
-
-
-
-type Box = (Grid, Range)
-
--- | Boxes contain either more boxes or values
-data Unpack = Boxes [Box] | 
-
--- | Unpack one level of a range.
-unpack :: (Grid, Range) -> [(Grid, Range)]
-unpack (grid, (HTuple range:ranges)) = (unpack (keepLeft (length range)) grid) ++ (unpack (dropLeft (length range) grid) (HTuple ranges))
-unpack (grid, (VTuple range:ranges)) = (unpack (keepDown (length range)) grid) ++ (unpack (dropDown (length range) grid) (VTuple ranges))
-unpack (grid, (HList range)) = (
-unpack (grid, (VList range)) = 
-unpack (grid, (Cell sorter)) = sorter $ value (keepLeft 1 grid $ keepDown 1 grid)
-
-tidytable :: Range
-tidytable = 
-type TidyTable =
-HList (VTuple [Cell, VList])
-
-
-validate :: Grammar -> Box -> Bool
-validate (Box grammar) box
-  | height grammar == height box && width box == width box = all $ mapGrid validate box
-  | otherwise = False
-validate (LeftFill grammar) bigBox
-  | height grammar == height box = validate grammar box && validate rest
-  | width grammar == 0 || height fill == 0 = True
-  | otherwise = False
-  where
-    box, rest = splitFill grammar bigBox
-validate (DownFill grammar) bigBox
-  | width grammar == width grid = validate grammar box && validate rest
-  | width grammar == 0 || height fill == 0 = True
-  | otherwise = False
-  where
-    box, rest = splitFill grammar bigBox
-
-splitFill :: Fill -> Box -> (Box, Fill)
-splitFill (LeftFill fill) box = takeLeft (width box) fill
-splitFill (DownFill fill) box = takeDown (width box) fill
+-}
 
 main = do putStr "3"
