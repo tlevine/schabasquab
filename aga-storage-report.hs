ade34fe167b7c9a7bc2189221385362a3e53b626
Thomas Levine <_@thomaslevine.com>
2014-12-26 23:43:28 +0000

    filename

--- aga-storage-report.hs
+++ aga-storage-report.hs
@@ -4,11 +4,12 @@ import Codec.Xlsx
 import qualified Data.ByteString.Lazy as L
 import Control.Lens
 
+filename = "examples/scott_neal__37897__Aga4-7.xlsx"
+
 main :: IO ()
 main = do
-  bs <- L.readFile "report.xlsx"
+  bs <- L.readFile filename
   let value = toXlsx bs ^? ixSheet "List1" .
               ixCell (3,2) . cellValue . _Just
   putStrLn $ "Cell B3 contains " ++ show value
 
-filename = "scott_neal__37897__Aga4-7.xlsx"
