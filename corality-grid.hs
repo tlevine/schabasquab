12aad4efd591eeb6c50405a77756489059689eb5
Thomas Levine <_@thomaslevine.com>
2015-01-15 15:44:44 +0000

    write grid in haskell

--- corality-grid.hs
+++ corality-grid.hs
@@ -1,42 +1,44 @@
-"Header1",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal",,,,,,,"Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",
-"Normal",,,,,,"Off-sheet","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",
-"Normal",,,,,,," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header1",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info",,,,,," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info",,,,,," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,"Table Header",,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info","Off-sheet",,,," Table Header "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag ",
-"Normal","Unit / Info",,,,," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header1",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info",,,,,," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info",,,,," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info",,,,," Off-sheet "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info","Off-sheet",,,,,"Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",
-"Normal","Unit / Info",,,,,,"Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",
-"Normal","Unit / Info",,,,," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info","Table Header",,,," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info","Off-sheet",,,,," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info","Off-sheet",,,,," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag ",
-"Normal","Unit / Info","Off-sheet",,,," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Header2",,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info",,,,,," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - ",
-,,,,,,,,,,,,,,,,,,,,,,,,,,
-"Normal","Unit / Info",,,,,," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info","Table Header","Table Header",," Line Summary ","Empty_cell"," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info","Off-sheet","Normal",,," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",
-"Normal","Unit / Info",,,,," Empty_cell "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance ",
+-- Row-major
+grid = [ ["Header1","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","","","","","","","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",""]
+       , ["Normal","","","","","","Off-sheet","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",""]
+       , ["Normal","","","","","",""," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header1","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","","","","",""," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","","","","",""," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","Table Header","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","Off-sheet","","",""," Table Header "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag ",""]
+       , ["Normal","Unit / Info","","","",""," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header1","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","","","","",""," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","","","",""," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","","","",""," Off-sheet "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","Off-sheet","","","","","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",""]
+       , ["Normal","Unit / Info","","","","","","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal","Normal",""]
+       , ["Normal","Unit / Info","","","",""," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","Table Header","","",""," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","Off-sheet","","","",""," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","Off-sheet","","","",""," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag "," Flag ",""]
+       , ["Normal","Unit / Info","Off-sheet","","",""," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Header2","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","","","","",""," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - "," - ",""]
+       , ["","","","","","","","","","","","","","","","","","","","","","","","","","",""]
+       , ["Normal","Unit / Info","","","","",""," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","Table Header","Table Header",""," Line Summary ","Empty_cell"," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","Off-sheet","Normal","",""," Line Summary "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal "," Normal ",""]
+       , ["Normal","Unit / Info","","","",""," Empty_cell "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance "," Balance ",""]
+       ]
